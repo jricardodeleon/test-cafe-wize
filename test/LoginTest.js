848a5e25c2e49c84f5a6913b90dc2b779cd937bb
Ricardo <jricardodeleon@gmail.com>
2021-03-14 21:28:56 -0600

    Adding ShoppingCart

--- test/LoginTest.js
+++ test/LoginTest.js
@@ -10,17 +10,17 @@ fixture `Test Valid e Invalid Scenarios For Login`
     })
         
 
-test('Login With Valid Credentials', async t=>{
+test('1. Login With Valid Credentials', async t=>{
     await LoginPage.validLogin(CREDENTIALS.VALID_LOGIN.USERNAME, CREDENTIALS.VALID_LOGIN.PASSWORD)
     await t.expect(LoginPage.productosLabel.exists).ok()
 })
 
-test('Login With Invalid Credentials', async t=>{
+test('2. Login With Invalid Credentials', async t=>{
     await LoginPage.IvalidLogin(CREDENTIALS.INVALID_LOGIN.USERNAME, CREDENTIALS.INVALID_LOGIN.PASSWORD)
     await t.expect(LoginPage.errorMessage.exists).ok()
 })
 
-test('Logout From Products Page', async t=>{
+test('3. Logout From Products Page', async t=>{
     await LoginPage.logoutFromProducts(CREDENTIALS.VALID_LOGIN.USERNAME, CREDENTIALS.VALID_LOGIN.PASSWORD)
     await t.expect(LoginPage.loginLogo.exists).ok()
 })
